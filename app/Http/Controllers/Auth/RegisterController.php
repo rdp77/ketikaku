6959922c2995ef37a9f197033a5c854ef6bd1275
AzrieL/Muttq <denyprasetyo41@gmail.com>
2019-01-19 19:27:51 +0700

    1

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use DB;
-
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 class RegisterController extends Controller
 {
     /*
