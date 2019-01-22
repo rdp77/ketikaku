716bbdf5d89ddd27c1ae505ac0f35df6d8ce11cb
AzrieL/Muttq <denyprasetyo41@gmail.com>
2019-01-23 03:55:25 +0700

    Update profileController.php

--- app/Http/Controllers/backend_controller/profileController.php
+++ app/Http/Controllers/backend_controller/profileController.php
@@ -12,8 +12,8 @@ class profileController extends Controller
 {
     public function profile($name)
     {  
-    	$novel = DB::table('d_novel')->where('dn_created_by',Auth::user()->id)->orderBy('dn_id','DESC')->get();
-    	$q_total_book = DB::table('d_novel')->where('dn_created_by',Auth::user()->id)->get();
+    	$novel = DB::table('d_novel')->where('dn_created_by',Auth::user()->m_id)->orderBy('dn_id','DESC')->get();
+    	$q_total_book = DB::table('d_novel')->where('dn_created_by',Auth::user()->m_id)->get();
     	$total_book = count($q_total_book);
         return view('backend_view.master_user.profile_detail',compact('total_book','novel'));
     }
