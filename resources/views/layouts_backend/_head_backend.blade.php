7790a4744dcaadaa5a86d67d1b8d24e240748052
AzrieL/Muttq <denyprasetyo41@gmail.com>
2019-04-02 22:46:48 +0700

    Update _head_backend.blade.php

--- resources/views/layouts_backend/_head_backend.blade.php
+++ resources/views/layouts_backend/_head_backend.blade.php
@@ -3,8 +3,8 @@
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <!-- Tell the browser to be responsive to screen width -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="">
-<meta name="author" content="">
+<meta name="description" content="APASIHH">
+<meta name="author" content="DENYPRASS">
 <!-- Favicon icon -->
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets_backend/images/favicon.png') }}">
 <title>KETIKAKU</title>
