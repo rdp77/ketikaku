c5f3dd21c9552b2db004a5720ec2b3e0717c8b43
AzrieL/Muttq <denyprasetyo41@gmail.com>
2019-04-02 23:21:21 +0700

    Update _head_frontend.blade.php

--- resources/views/layouts_frontend/_head_frontend.blade.php
+++ resources/views/layouts_frontend/_head_frontend.blade.php
@@ -3,12 +3,13 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta name="description" content="ESENSI CREATIVE.">
-        <meta name="author" content="ESENSI ">
+        <meta name="author" content="ESENSI">
         <meta name="keyword" content="ketikau, novel, chapter, magazine">
         <!-- Shareable -->
         <meta property="og:title" content="ESENSI CREATIVE" />
-        <meta property="og:type" content="ESENSI" />
-        <meta property="og:url" content="ketikaku.com" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="http://ketikaku.com/" />
+        <meta property="og:description"        content="How much does culture influence creative thinking?" />
         <meta property="og:image" content="{{ asset('assets_backend/images/favicon.png') }}" />
         <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets_backend/images/favicon.png') }}">
         <title>KETIKAKU</title>
