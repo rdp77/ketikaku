acd02d6c9bea6e60a5944cb03e2e8d7bfcef509c
AzrieL/Muttq <denyprasetyo41@gmail.com>
2019-04-11 21:38:22 +0700

    1

--- app/Http/Controllers/mail/verify_emailController.php
+++ app/Http/Controllers/mail/verify_emailController.php
@@ -17,7 +17,7 @@ class verify_emailController extends Controller
     	$email = Auth::user()->m_email;
     	$username = Auth::user()->m_username;
     	$token = Auth::user()->m_token;
-    	$code = Auth::user()->m_id;
+    	$code = Auth::user()->m_code;
 
     	$mail = Mail::send('mail.mail_verification', 
                     ['username' => $username,
